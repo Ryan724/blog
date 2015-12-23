484c0443bd728cbc4f804502f81c029cb13208c5
zhengpengjie <zhengpengjie@beisen.com>
2015-12-24 00:24:23 +0800

    有架构了~

--- blog03/apibox/commands/api.js
+++ blog03/apibox/commands/api.js
@@ -1,11 +1,11 @@
 // 依赖模块
 var path = require('path');
 var fs = require('fs');
+var server = require('./lib/server');
 
 // 导出命令
 module.exports = {
     exec: function(argv) {
-        console.log(argv.i)
         var cwd = process.cwd();
         if (argv.init || argv.i) {
             var root = argv.init || argv.i;
